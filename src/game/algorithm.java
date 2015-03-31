eaa55c2ba88f136103c721775bdc2afbec17a096
CDandu16 <chaitudandu@gmail.com>
2015-03-30 21:04:39 -0400

    I made the game much more optimized, fixed some glitches. added an array list so that way people can't use the same word again and again. Also put comments in the code:)

--- src/game/algorithm.java
+++ src/game/algorithm.java
@@ -1,30 +1,38 @@
 package game;
 
+import com.sun.org.apache.xerces.internal.xs.StringList;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class algorithm {
+
 	public static void main(String[] args){
+		List list = new ArrayList();
 		boolean game = true;
 		String nextWord = null;
 		String word = null;
 		while(game){
-		Scanner sc = new Scanner(System.in);
-			if(nextWord == null){
+			Scanner sc = new Scanner(System.in);
+			if(word == null){
 				System.out.println("Start game");
-				word = sc.next();
-			}else if(word.charAt(word.length()-word.length()) != (nextWord.charAt(nextWord.length() - 1))){
-				
-				System.out.println("you lose 1");
+				word = sc.next();//starts the game with first word
+				list.add(word);//adds Player ones word to list so that duplicates are avoided
+			}else if((word.charAt(0) != (nextWord.charAt(nextWord.length() - 1))) || list.contains(word)){
+				System.out.println("you lose player 1");
 				game = false;
 			}
-		System.out.println("The next word must start with " +  word.charAt(word.length() - 1));
-		nextWord = sc.next();
-			if(nextWord.charAt(nextWord.length()-nextWord.length()) != (word.charAt(word.length() - 1))){
-				System.out.println("you lose 2");
+			list.add(word);
+			System.out.println("Player 2: The next word must start with " +  word.charAt(word.length() - 1));
+			nextWord = sc.next();//takes player 2's input word
+			if((nextWord.charAt(0) != (word.charAt(word.length() - 1))) || list.contains(nextWord)){
+				System.out.println("you lose player 2");
 				game = false;
 			}else{
-			System.out.println("The next word must start with " +  nextWord.charAt(nextWord.length() - 1));
-			word = sc.next();
+				list.add(nextWord);//adds player's 2 word to the list if its successful.
+				System.out.println("Player 1: The next word must start with " +  nextWord.charAt(nextWord.length() - 1));
+				word = sc.next();
 			}
 		}
 	}
